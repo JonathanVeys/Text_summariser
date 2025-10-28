aea2a92a9c6be723177c4df492e59eb18e2087ed
Jonathan Veys <Jonathancr1234@gmail.com>
2025-10-28 23:19:20 +0000

    Requirements added

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+REPO_NAME = "Text_summariser"
+AUTHOR_USER_NAME = "JonathanVeys"
+SRC_REPO = "textSummariser"
+AUTHOR_EMAIL = "JonathanCr1234@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for a NLP app",
+    long_description=long_description,
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "bug_tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
